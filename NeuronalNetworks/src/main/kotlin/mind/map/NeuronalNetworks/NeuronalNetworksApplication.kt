e1a70920af57ae1040764269929bcd4c285b32f8
domeeno <dominicflocea.dev@gmail.com>
2023-04-19 15:53:38 +0300

    init topic connection service. add .idea gitignore

---
+++ NeuronalNetworks/src/main/kotlin/mind/map/NeuronalNetworks/NeuronalNetworksApplication.kt
@@ -0,0 +1,11 @@
+package mind.map.NeuronalNetworks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeuronalNetworksApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeuronalNetworksApplication>(*args)
+}
